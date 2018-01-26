26e0fec6b243c996b6a72a1abd836cf87a29c0bc
Sabrina Seibel <seibelsabrina@gmail.com>
2018-01-25 20:38:09 -0500

    add person 1 file

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Sabrina Seibel
  *  @version 1.1
  */
 public class Person1 {
@@ -30,9 +30,9 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
-	}
+    	String newstring = input.substring(2) + input.substring(0,2);
+    	return newstring;
+  }
 	
 	/**
 	 * Return a string rep of this object
